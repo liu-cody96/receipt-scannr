6958d02056f14a20cc75c52d765906ea84c5d73f
Aubrey Ross <87405979+aubreyross@users.noreply.github.com>
2023-03-08 03:26:22 -0600

    database files

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema ({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    userId: {
+        type: String,
+        unique: true,
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
